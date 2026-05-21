5a59270aa20bc51d00f08106980b6fafed768ce4
MohamedGonem <nightangelyt01@gmail.com>
2026-05-21 18:54:24 +0300

    impl default() to Session Struct

--- src/questions.rs
+++ src/questions.rs
@@ -4,3 +4,14 @@ pub struct Session {
     pub sessions_time: Vec<usize>,
     pub break_length: usize,
 }
+
+impl Default for Session {
+    fn default() -> Self {
+        Self {
+            sessions_count: 5,
+            session_length: 5,
+            sessions_time: vec![25, 20, 15, 10, 5],
+            break_length: 5,
+        }
+    }
+}
