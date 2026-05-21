dab40ec1a588fa27569ce7fcf414d2b426a9c51f
MohamedGonem <nightangelyt01@gmail.com>
2026-05-21 18:53:23 +0300

    add body, icon, urgency fn to add data into struct

--- src/notfication.rs
+++ src/notfication.rs
@@ -16,4 +16,24 @@ impl Notification {
             icon: None,
         }
     }
+
+    pub fn body(mut self, body: impl Into<String>) -> Self {
+        self.body = Some(body.into());
+        self
+    }
+
+    pub fn urgency(mut self, urgency: impl Into<String>) -> Self {
+        self.urgency = Some(urgency.into());
+        self
+    }
+
+    pub fn timeout(mut self, ms: u32) -> Self {
+        self.timeout = Some(ms);
+        self
+    }
+
+    pub fn icon(mut self, icon: impl Into<String>) -> Self {
+        self.icon = Some(icon.into());
+        self
+    }
 }
