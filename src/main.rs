0ba6ce76d67ccd0ee10eeed4fac142837476686b
MohamedGonem <nightangelyt01@gmail.com>
2026-05-21 19:00:35 +0300

    add main loop and notify after program finish

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,12 @@ mod notfication;
 mod questions;
 
 fn main() {
+    loop {
+        let session = questions::ask();
+        let mut current_session: usize = 0;
+    }
+
+    notfication::Notification::new("Pomodoro")
+        .body("All done! Great work today")
+        .send();
 }
