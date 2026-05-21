95c484f3880bba432167d849c8473d54687ac33d
MohamedGonem <nightangelyt01@gmail.com>
2026-05-21 19:01:03 +0300

    add pomodoro loop logic

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,35 @@ fn main() {
     loop {
         let session = questions::ask();
         let mut current_session: usize = 0;
+        for session_time in session.sessions_time.iter() {
+            notfication::Notification::new("Pomodoro Started")
+                .body(&format!("Focus for {session_time} minutes"))
+                .urgency("normal")
+                .icon("clock")
+                .send();
+
+            std::thread::sleep(std::time::Duration::from_secs(
+                session.session_length as u64 * 60,
+            ));
+            current_session += 1;
+            if !loop_questions::ask_continue(current_session, session.sessions_count) {
+                break;
+            }
+        }
+        notfication::Notification::new("Pomodoro Done!")
+            .body(&format!("Take a {} minute break", session.break_length))
+            .urgency("critical")
+            .timeout(0)
+            .icon("dialog-information")
+            .send();
+
+        std::thread::sleep(std::time::Duration::from_secs(
+            session.break_length as u64 * 60,
+        ));
+
+        if !loop_questions::ask_new() {
+            break;
+        }
     }
 
     notfication::Notification::new("Pomodoro")
